3789064ae2af163a409be5367aeacafd48349c35
Deadfirer <alexandrefaust1@hotmail.com>
2019-09-25 09:29:50 -0300

    Iniciando o Projeto

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Iniciando Projeto, Verificando os Itens</h1>
+    </div>
+  );
+}
+
+export default App;
